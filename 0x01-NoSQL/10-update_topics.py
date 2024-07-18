a375497a3f7ca9d84193b9405197b02ea86c593d
chapaedb <chapaeresso1@gmail.com>
2024-07-19 01:16:26 +0300

    Update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -10,5 +10,5 @@ def update_topics(mongo_collection, name, topics):
 
     mongo_collection.update(
         {'name': name },
-        {'$set': {'topic': topics}}
+        {'$set': {'topics': topics}}
     )
