9f09b5c1b2487518a0271170e509f5bd76b0aacd
chapaedb <chapaeresso1@gmail.com>
2024-07-19 01:18:48 +0300

    Update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,14 +1,12 @@
-#!/usr/bin/ env python3
-"""
-Task 10
-"""
+#!/usr/bin/env python3
+'''Task 10's module.
+'''
+
 
 def update_topics(mongo_collection, name, topics):
-    """
-    Updates topics based on the name
-    """
-
+    '''Changes all topics of a collection's document based on the name.
+    '''
     mongo_collection.update_many(
-        {'name': name },
+        {'name': name},
         {'$set': {'topics': topics}}
     )
