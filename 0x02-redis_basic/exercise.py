5283107c04b3bf39f1c2e9e5a51964d5c461cad4
unknown <chapaeresso1@gmail.com>
2024-08-05 23:33:53 +0300

    Update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -33,6 +33,7 @@ def call_history(method: Callable) -> Callable:
         return output
     return wrapper
 
+def replay(fn: Callable) -> None:
     """ Check redis for how many times a function was called and display:
             - How many times it was called
             - Function args and output for each call
